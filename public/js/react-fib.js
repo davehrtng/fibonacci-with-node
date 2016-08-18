5ff6c0cae6a588b0d6e407efc362c9f86708c801
davehrtng <david.e.harting@gmail.com>
2016-08-17 21:35:27 -0400

    complete react conversion

--- public/js/react-fib.js
+++ public/js/react-fib.js
@@ -1,34 +1,38 @@
 var Fibonacci = React.createClass({
   getInitialState: function() {
-    return {sequence: null, value: null};
+    return {sequence: 1, value: 0};
+  },
+
+  handleSequenceChange: function(e) {
+    this.setState({sequence: e.target.value})
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    $.get(this.props.url + "/" + this.state.sequence, function(dataJsonString, status, jqXHR){
+      var data = JSON.parse(dataJsonString);
+      this.setState({value: data.fibNumber})
+    }.bind(this));
   },
 
   render: function() {
-    return <h1>{this.state.value}</h1>
+    return (
+      <div>
+        <h1>Fibonacci</h1>
+        <h3><small>Compute some fibonacci numbers.</small></h3>
+        <form className="form-inline" onSubmit={this.handleSubmit}>
+          <input type="number" className="form-control" min="1" max="1250"
+            onChange={this.handleSequenceChange}
+            value={this.state.sequence} />
+          <input type="submit" className="btn btn-primary" value="Ok" />
+        </form>
+        <h1>{this.state.value}</h1>
+      </div>
+    );
   }
 });
 
 ReactDOM.render(
-  <Fibonacci />,
+  <Fibonacci url="fib" />,
   document.getElementById('react-node')
 );
-
-
-// jquery to do some basic stuff
-$(document).ready(function() {
-  $('#form-fib').submit(function () {
-    var inputSequenceNumber = $('#input-sequence-number');
-    var sequenceNumber = inputSequenceNumber.val();
-    if(sequenceNumber != '') {
-      var fibServiceUrl = "fib/" + sequenceNumber;
-
-      $.get(fibServiceUrl, function(dataJsonString, status, jqXHR){
-        var data = JSON.parse(dataJsonString);
-
-        $('#h1-output').text(data.fibNumber);
-        inputSequenceNumber.select();
-      });
-    }
-    return false;
-  });
-});
