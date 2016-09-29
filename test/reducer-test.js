a73f8f4ee843005ecf737bc7d41c7285ea061878
David Harting <david.e.harting@gmail.com>
2016-09-29 17:46:20 +0000

    skeleton of tests and run tests with npm script

---
+++ test/reducer-test.js
@@ -0,0 +1,18 @@
+/**
+ * Tests of redux reducers go here to make sure state management works correctly
+ */
+ 
+ const assert = require('assert');
+ 
+ describe('Reducers', () => {
+    describe('No action', function() {
+        it('should return the default state', () => {
+            assert.fail();
+        });
+    })
+    describe('ADD FIB', () => {
+        it('should not add duplicates', () => {
+            assert.fail();
+        });
+    });
+ });
